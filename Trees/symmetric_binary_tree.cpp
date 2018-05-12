5fe5a129532b9fee15aed0eba6f3e706191de1e0
manp3104 <manishpal.uee2015@iitr.ac.in>
2018-05-12 03:37:59 +0000

    Amazon

---
+++ Trees/symmetric_binary_tree.cpp
@@ -0,0 +1,46 @@
+//  https://www.interviewbit.com/problems/symmetric-binary-tree/
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+vector<int> inOrder(TreeNode* A) {
+    vector<int> data;
+    if(!A)
+        return data;
+    stack<TreeNode*> st;
+    while(1) {
+        while(A) {
+            st.push(A);
+            A = A->left;
+        }
+        if(st.empty())
+            break;
+        A = st.top();
+        st.pop();
+        data.push_back(A->val);
+        A = A->right;
+    }
+    return data;
+}
+int isPalin(vector<int> data) {
+    int n = data.size();
+    for(int i=0;i<n/2;i++) {
+        if(data[i]!=data[n-1-i])
+            return 0;
+    }
+    return 1;
+}
+int Solution::isSymmetric(TreeNode* A) {
+    vector<int> data = inOrder(A);
+    //cout << data.size();
+    if(isPalin(data))
+        return 1;
+    return 0;
+}
