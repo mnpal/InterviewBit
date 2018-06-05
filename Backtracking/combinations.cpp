c848c4aed3f1d5babb892d8cc5438556e46aba8c
Manish Kumar Pal <manishpal.uee2015@iitr.ac.in>
2018-06-06 00:07:57 +0530

    Adobe, Amazon
    Added alternative solution

--- Backtracking/combinations.cpp
+++ Backtracking/combinations.cpp
@@ -25,3 +25,25 @@ vector<vector<int> > Solution::combine(int n, int B) {
     return result;
 }
 
+######## ANOTHER SOLUTION ###########
+
+vector<vector<int> > Solution::combine(int A, int B) {
+    vector<vector<int> > result;
+    
+    auto lim = pow(2, A);
+    
+    for(auto i=0;i<lim;i++) {
+        vector<int> temp;
+        for(auto j=0;j<A;j++) {
+            if(i&(1<<j))
+                continue;
+            temp.push_back(j+1);
+        }
+        if(temp.size()==B) {
+            sort(temp.begin(), temp.end());
+            result.push_back(temp);
+        }
+    }
+    sort(result.begin(), result.end());
+    return result;
+}
