121bb748eaf25240c5ea043d272e349c8e99600d
manp3104 <manishpal.uee2015@iitr.ac.in>
2018-06-19 02:00:35 +0530

    Google, Amazon

--- Hashing/valid_sudoku.cpp
+++ Hashing/valid_sudoku.cpp
@@ -83,3 +83,94 @@ int Solution::isValidSudoku(const vector<string> &A) {
     return 0;
 }
 
+
+
+############ OR ###########
+
+
+void clearMap(unordered_map<char, int> &mp) {
+    unordered_map<char, int>::iterator it = mp.begin();
+    while(it!=mp.end()) {
+        mp.erase(it);
+        it=mp.begin();
+    }
+}
+
+bool validRow(const vector<string> &A) {
+    int n = A.size();
+    
+    for(int i=0;i<n;i++) {
+        unordered_map<char, int> mp;
+        for(int j=0;j<n;j++) {
+            if(A[i][j]!='.') {
+                if(A[i][j]-'0'<0 || A[i][j]-'0'>9 || mp.find(A[i][j])!=mp.end())
+                    return false;
+                else
+                    mp.insert(make_pair(A[i][j], 1));
+            }
+        }
+        clearMap(mp);
+    }
+    return true;
+}
+
+bool validColumn(const vector<string> &A) {
+    int n = A.size();
+    
+    for(int i=0;i<n;i++) {
+        unordered_map<char, int> mp;
+        for(int j=0;j<n;j++) {
+            if(A[j][i]!='.') {
+                if(A[j][i]-'0'<0 || A[j][i]-'0'>9 || mp.find(A[j][i])!=mp.end())
+                    return false;
+                else
+                    mp.insert(make_pair(A[j][i], 1));
+            }
+        }
+        /*
+        for(auto it=mp.begin();it!=mp.end();it++)
+            cout << it->first << " ";
+        cout << endl;
+        */
+        clearMap(mp);
+    }
+    return true;
+}
+
+bool validGrid(const vector<string> &A) {
+    int n = A.size();
+    int a;
+    for(int k=0;k<n;k++) {
+        if(k<3)
+            a = 0;
+        else if(k<6)
+            a = 3;
+        else 
+            a = 6;
+        unordered_map<char, int> mp;
+        for(int i=a;i<a+3;i++) {
+            for(int j=3*(k%3);j<3*(k%3)+3;j++) {
+                if(A[i][j]!='.') {
+                    if(A[i][j]-'0'<0 || A[i][j]-'0'>9 || mp.find(A[i][j])!=mp.end())
+                        return false;
+                    else
+                        mp.insert(make_pair(A[i][j], 1));
+                }
+            }
+        }
+        
+        
+        clearMap(mp);
+    }
+    return true;
+}
+
+int Solution::isValidSudoku(const vector<string> &A) {
+    
+    if(validRow(A) && validColumn(A) && validGrid(A))
+        return 1;
+    return 0;
+    
+}
+
+
