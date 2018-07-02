989172a84462b46ec7b35ee9b3f257047bf340fa
manp3104 <manishpal.uee2015@iitr.ac.in>
2018-07-03 00:24:14 +0530

    Added another solution for evaluate_expression

--- Stacks And Queues/evaluate_expression.cpp
+++ Stacks And Queues/evaluate_expression.cpp
@@ -36,3 +36,58 @@ int Solution::evalRPN(vector<string> &A) {
     return res;
 }
 
+
+######### OR ###########
+
+void print(stack<int> st) {
+    while(!st.empty()) {
+        cout << st.top() << " ";
+        st.pop();
+    }
+    cout << endl;
+}
+int Solution::evalRPN(vector<string> &A) {
+    
+    stack<int> st;
+    
+    auto i = 0;
+    auto n = A.size();
+    
+    while(i<n) {
+        int n1 = 0;
+        int n2 = 0;
+        if(A[i]!="/" && A[i]!="*" && A[i]!="+" && A[i]!="-") {
+            stringstream ss(A[i]);
+            auto x = 0;
+            ss >> x;
+            st.push(x);
+            //cout << "it has been executed\n";
+        }
+        else {
+            //print(st);
+            n1 = st.top();
+            st.pop();
+            n2 = st.top();
+            st.pop();
+                
+            switch(A[i][0]) {
+                case '+': st.push(n1+n2);
+                    break;
+                case '-': st.push(n2-n1);
+                    break;
+                case '*': st.push(n2*n1);
+                    break;
+                case '/': st.push(n2/n1);
+                    break;
+                default:
+                    return 0;
+            }
+        }
+        
+        i++;
+    }
+    if(st.empty())
+        return 0;
+    return st.top();
+}
+
