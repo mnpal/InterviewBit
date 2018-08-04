1eeacf8c0222f28fec8d163c5d9866296ded565f
manp3104 <manishpal.uee2015@iitr.ac.in>
2018-08-04 16:23:27 +0530

    Google

--- Arrays/merge_overlapping_intervals.cpp
+++ Arrays/merge_overlapping_intervals.cpp
@@ -51,3 +51,48 @@ vector<Interval> Solution::merge(vector<Interval> &A) {
     return result;
 }
 
+
+
+################# OR ####################
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+bool myComp(Interval a, Interval b) {
+    return a.start < b.start;
+}
+vector<Interval> Solution::merge(vector<Interval> &A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    
+    if(A.size()<2)
+        return A;
+    
+    sort(A.begin(), A.end(), myComp);
+    vector<Interval> result;
+    Interval temp = A[0];
+    
+    for(int j=1;j<A.size();j++) {
+        // overlaps
+        if(temp.end >= A[j].start) {
+            temp.end = max(temp.end, A[j].end);
+        }
+        // no overlap
+        else {
+            result.push_back(temp);
+            temp = A[j];
+        }
+        
+    }
+    result.push_back(temp);
+    return result;
+}
+
+
