43a5e31267a53a801e9a9949cf79e3d8ded68a7b
manp3104 <manishpal.uee2015@iitr.ac.in>
2018-08-06 00:13:40 +0530

    Backtracking solution

--- Backtracking/combination_sum_ii.cpp
+++ Backtracking/combination_sum_ii.cpp
@@ -34,3 +34,39 @@ vector<vector<int> > Solution::combinationSum(vector<int> &A, int B) {
     
 }
 
+
+########## ANOTHER SOLUTION ##########
+
+void getSets(int sum, int index, vector<int> &A, vector<int> &current, set<vector<int> > &result) {
+    if(sum==0) {
+        result.insert(current);
+        return;
+    }
+    if(index>=A.size() || sum<0)
+        return;
+    current.push_back(A[index]);
+    getSets(sum-A[index], index+1, A, current, result);
+    current.pop_back();
+    getSets(sum, index+1, A, current, result);
+}
+
+vector<vector<int> > Solution::combinationSum(vector<int> &A, int B) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    
+    vector<int> a;
+    sort(A.begin(), A.end());
+            
+    set<vector<int> > result;
+    vector<int> current;
+    getSets(B, 0, A, current, result);
+    
+    vector<vector<int> > ans;
+    for(auto it: result)
+        ans.push_back(it);
+    
+    return ans;
+}
+
