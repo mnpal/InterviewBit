1bb5a1ceef2cf8026f30115149d28cb2ac6de58e
manp3104 <manishpal.uee2015@iitr.ac.in>
2018-08-05 20:40:32 +0530

    Backtracking solutions

--- Backtracking/subset.cpp
+++ Backtracking/subset.cpp
@@ -19,3 +19,29 @@ vector<vector<int> > Solution::subsets(vector<int> &A) {
     return res;
 }
 
+
+########## OR ##############
+
+void generateSets(vector<int> &A, int index, vector<int> &set, vector<vector<int> > &sets) {
+    if(index >= A.size()) {
+        sets.push_back(set);
+        return;
+    }
+    //ignore the current index
+    generateSets(A, index+1, set, sets);
+    //include the current index
+    set.push_back(A[index]);
+    generateSets(A, index+1, set, sets);
+    set.pop_back();
+}
+vector<vector<int> > Solution::subsets(vector<int> &A) {
+
+    vector<int> set;
+    vector<vector<int> > sets;
+    sort(A.begin(), A.end());
+    generateSets(A, 0, set, sets);
+    sort(sets.begin(), sets.end());
+    return sets;
+}
+
+
--- Backtracking/subset_ii.cpp
+++ Backtracking/subset_ii.cpp
@@ -26,3 +26,30 @@ vector<vector<int> > Solution::subsetsWithDup(vector<int> &A) {
     return res;
 }
 
+
+############## OR ###################3
+
+void generateSet(vector<int> &A, int index, vector<int> &current, set<vector<int> > &sets) {
+    if(index >= A.size()) {
+        sets.insert(current);
+        return;
+    }    
+    generateSet(A, index+1, current, sets);
+    current.push_back(A[index]);
+    generateSet(A, index+1, current, sets);
+    current.pop_back();
+}
+vector<vector<int> > Solution::subsetsWithDup(vector<int> &A) {
+    sort(A.begin(), A.end());
+    
+    vector<vector<int> > result;
+    vector<int> current;
+    set<vector<int> > sets;
+    
+    generateSet(A, 0, current, sets);
+    
+    for(auto set: sets)
+        result.push_back(set);
+    return result;
+}
+
