043718cb875888c1a662c5eefb3d57cfce74452d
Manish Kumar Pal <manishpal.uee2015@iitr.ac.in>
2018-08-16 05:38:24 +0530

    Added alternative solution

--- Bit Manipulation/number_of_1_bits.cpp
+++ Bit Manipulation/number_of_1_bits.cpp
@@ -15,3 +15,19 @@ int Solution::numSetBits(unsigned int n) {
     return count;
 }
 
+
+######### OR #########
+
+int Solution::numSetBits(unsigned int A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    int bits = 0;
+    
+    for(int i=0;i<32;i++)
+        if(A&(1<<i))
+            bits++;
+    return bits;
+}
+
