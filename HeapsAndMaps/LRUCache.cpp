525c9440560e1e75691b4e09fa4d5e472e2428c9
manp3104 <manishpal.uee2015@iitr.ac.in>
2018-08-22 08:17:15 +0530

    Added alternative solution

--- HeapsAndMaps/LRUCache.cpp
+++ HeapsAndMaps/LRUCache.cpp
@@ -43,3 +43,50 @@ void LRUCache::set(int key, int value) {
     mp[key] = value;
 }
 
+
+########### OR ############
+
+
+#include <list>
+using namespace std;
+
+map<int, int> mp;
+list<int> lst;
+int sz;
+
+LRUCache::LRUCache(int capacity) {
+    sz = capacity;
+    mp.clear();
+    lst.clear();
+}
+
+int LRUCache::get(int key) {
+    if(mp.find(key)==mp.end())
+        return -1;
+    
+    lst.remove(key);
+    lst.push_front(key);
+    return mp[key];
+}
+
+void LRUCache::set(int key, int value) {
+    if(mp.find(key)!=mp.end()) {
+        mp[key] = value;
+        lst.remove(key);
+        lst.push_front(key);
+        return;
+    }
+    
+    if(mp.size() < sz) {
+        mp[key] = value;
+        lst.push_front(key);
+        return;
+    }
+    
+    auto it = mp.find(lst.back());
+    mp.erase(it);
+    lst.pop_back();
+    lst.push_front(key);
+    mp[key] = value;
+}
+
