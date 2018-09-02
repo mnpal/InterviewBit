875e9f3a6e73c79d49f00e9932295192ad4a09c0
manp3104 <manishpal.uee2015@iitr.ac.in>
2018-09-02 07:03:50 +0530

    Facebook, Microsoft, Amazon

--- Linked Lists/reverse_linked_list_II.cpp
+++ Linked Lists/reverse_linked_list_II.cpp
@@ -8,71 +8,46 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-int findLen(ListNode* A) {
-    int c = 0;
-    while(A!=NULL) {
-        c++;
-        A = A->next;
-    }
-    return c;
-}
 ListNode* Solution::reverseBetween(ListNode* A, int B, int C) {
     
-    ListNode* prevNode = A;
+    if(!A || !A->next)
+        return A;
     
-    for(int i=1;i<B-1;i++) {
-        prevNode = prevNode->next;
+    ListNode* curr, *prev, *j1, *j2, *temp, *head;
+    head = A;
+    int k = 1;
+    curr = A;
+    prev = NULL;
+    
+    while(k<B) {
+        prev = curr;
+        curr = curr->next;
+        k++;
     }
+    j1 = curr;
     
-    ListNode* nextNode = A;
-    for(int i=0;i<C;i++) {
-        if(nextNode->next==NULL)
-            break;
-        nextNode = nextNode->next;
+    while(k<C) {
+        curr = curr->next;
+        k++;
     }
+    j2 = curr;
     
-    vector<ListNode*> nodes;
-    ListNode* temp;
+    if(prev)
+        prev->next = j2;
+    prev = j2->next;
+    
+    curr = j1;
+    temp = j1->next;
+    
+    while(prev!=j2) {
+        curr->next = prev;
+        prev = curr;
+        curr = temp;
+        if(temp!=j2)
+            temp = temp->next;
+    }
     if(B==1)
-        temp = A;
-    else
-        temp = prevNode->next;
-        
-    for(int i=B-1;i<C;i++) {
-        nodes.push_back(temp);
-        if(temp->next==NULL)
-            break;
-        temp = temp->next;
-    }
-    
-    reverse(nodes.begin(), nodes.end());
-    
-    int n = C-B;
-    int m = findLen(A);
-    
-    for(int i=0;i<n;i++) {
-        nodes[i]->next = nodes[i+1];
-    }
-    
-    if(B==1 && C==m) {
-        nodes[n]->next = NULL;
-        return nodes[0];
-    }
-    else if(B==1) {
-        nodes[n]->next = nextNode;
-        return nodes[0];
-    }
-    else if(C==m) {
-        prevNode->next = nodes[0];
-        nodes[n]->next = NULL;
-        return A;
-    }
-    else {
-        prevNode->next = nodes[0];
-        nodes[n]->next = nextNode;
-        return A;
-    }
-    
-    return A;
+        head = j2;
+    return head;
 }
 
