a1cbe4b3923ad7488018e43ef8201b7e5ed44807
manp3104 <manishpal.uee2015@iitr.ac.in>
2018-09-02 06:05:42 +0530

    Microsoft

--- Linked Lists/swap_list_nodes_in_pairs.cpp
+++ Linked Lists/swap_list_nodes_in_pairs.cpp
@@ -9,33 +9,31 @@
  * };
  */
 ListNode* Solution::swapPairs(ListNode* A) {
-    if(A==NULL || A->next==NULL)
-        return A;
-    ListNode* prev = A;
-    ListNode* curr = A->next;
-    ListNode* prevPrev;
     
-    prev->next = curr->next;
-    curr->next = prev;
-    A = curr;
-    
-    if(prev->next == NULL || prev->next->next==NULL)
+    if(!A || !A->next)
         return A;
     
-    prevPrev = prev;
-    prev = prev->next;
-    curr = prev->next;
-    
+    ListNode *curr, *prev, *pprev, *temp;
+    pprev = NULL;
+    prev = A;
+    curr = A->next;
+    temp = A->next->next;
+    A = A->next;
     
     while(1) {
-        prev->next = curr->next;
+        prev->next = temp;
         curr->next = prev;
-        prevPrev->next = curr;
-        if(prev->next==NULL || prev->next->next==NULL)
+        
+        if(pprev)
+            pprev->next = curr;
+        
+        if(!temp || !temp->next)
             break;
-        prevPrev = prev;
-        prev = prev->next;
-        curr = prev->next;
+        
+        pprev = prev;
+        prev = temp;
+        curr = temp->next;
+        temp = curr->next;
     }
     return A;
 }
