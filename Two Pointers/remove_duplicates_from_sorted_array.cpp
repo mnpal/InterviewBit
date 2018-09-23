78de8136554660d492e4c43e72cf1a25350e4266
manp3104 <manishpal.uee2015@iitr.ac.in>
2018-09-23 22:49:25 +0530

    Another solution

--- Two Pointers/remove_duplicates_from_sorted_array.cpp
+++ Two Pointers/remove_duplicates_from_sorted_array.cpp
@@ -45,3 +45,24 @@ int Solution::removeDuplicates(vector<int> &A) {
 }
 
 
+
+########## OR ##########
+
+
+int Solution::removeDuplicates(vector<int> &A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    
+    int i=0, j=1;
+    while(j<A.size()) {
+        if(A[i]!=A[j]) {
+            ++i;
+            A[i] = A[j];
+        }
+        else 
+            ++j;
+    }
+    return i+1;
+}
